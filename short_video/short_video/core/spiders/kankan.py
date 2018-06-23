75ab061178fc90d388aeabd99244e73669c31396
4000 <zou4000@126.com>
2018-06-23 19:14:18 +0800

    xxx

--- short_video/short_video/core/spiders/kankan.py
+++ short_video/short_video/core/spiders/kankan.py
@@ -9,10 +9,23 @@ from core.search_tools import SearchBaiduHotWordTool
 # START_TIME = int(time()) - 24 * 60 * 60 * 2 # 这样就是两天内的数据
 START_TIME = 0
 
+# 这里设置翻页到的最早时间，超过这个时间就不继续翻页
+
+FILTER_TIME = int(time()) - 24 * 60 * 60 * 365
+
 
 class kankan(scrapy.Spider):
-    name = "kankan_app_search"
-    allowed_domains = ['xinhuanet.com', 'news.cn']
+    name = "kankan"
+    allowed_domains = ['kankanews.com']
+
+    templates = [
+        # 注意都有一个%s
+        # 用于生成spider的访问链接
+        # 填充的都是timestamp
+        'http://baoliao.api.kankanews.com/kkuser/listinfo/list/appclassid/617/timestamp/%s?androidver=5.2.6',  # 视频
+        # 精选
+        'http://baoliao.api.kankanews.com/kkuser/listinfo/list/appclassid/618/timestamp/%s?androidver=5.2.6'  # 深度
+    ]
 
     # http://api.app.kankanews.com/kankan/v5/kkappsearchv2?word==%E5%B3%B0%E4%BC%9A&type=all&page=1&size=20&stime=0&etime=1528976369
     def start_requests(self):
@@ -36,7 +49,10 @@ class kankan(scrapy.Spider):
 
     def getNextKeyWordRequest(self):
         keyword = self.SEARCH_TOOL.next()
-        return self.getSearchRequest(keyword, 0)
+        if keyword:
+            return self.getSearchRequest(keyword, 0)
+        else:
+            return None
 
     def getSearchRequest(self, keyword, pageNum):
         return scrapy.Request(
@@ -50,28 +66,76 @@ class kankan(scrapy.Spider):
     def parseSearchPage(self, response):
         data = json.loads(response.body)
         if data['err_code'] == 403:
-            yield self.getNextKeyWordRequest()
             return
             # parse json and save
         for video in data['list']:
-            if video['type'] != 'video':
-                print 'not video'
+            item = self.parseVideoProcess(video)
+            if not item:
                 continue
-            filelength = video['filelength']
-            times_array = filelength.split("'")
-            length = int(times_array[0]) * 6 + int(times_array[1])
-            item = {
-                'title': video['title'],
-                'url': video['titleurl'].replace('\\', ''),
-                'duration': length,
-                'img': video['titlepic'].replace('\\', ''),
-                'pub_time': datetime.datetime.strptime(video['newsdate'], '%Y-%m-%d'),
-                'source': 'kankan',
-                'type_': 'video'
-            }
-            print str(item['title'])
-            yield item
+            elif isinstance(item, dict):
+                yield item
+            elif isinstance(item, str) and item == 'break':
+                # todo: 后期不需要全量建立的时候，根据日期来判定终止条件，在parseVideoProcess里返回
+                break
+
         # next page
         pageNum = response.meta['pageNum'] + 1
         keyword = response.meta['keyword']
         yield self.getSearchRequest(keyword, pageNum)
+
+    def parseVideoProcess(self, video):
+        if video.get('type') != 'video' and video.get('videourl') == '':
+            # ps 有的text类型也有视频
+            return None
+        filelength = video.get('filelength').replace("''", '')
+        if "'" in filelength:
+            times_array = filelength.split("'")
+            length = int(times_array[0]) * 6 + int(times_array[1])
+        else:
+            length = int(filelength)
+        item = {
+            'type_' : 'video',
+            'title': video['title'],
+            'url': video['titleurl'].replace('\\', ''),
+            'duration': length,
+            'img': video['titlepic'].replace('\\', ''),
+            'pub_time': datetime.datetime.strptime(video['newsdate'], '%Y-%m-%d'),
+            'source': 'kankan',
+            'type_': 'video',
+            'description': video.get('intro', ''),
+            'newstime': video.get('newstime', 0)
+        }
+        print str(item['title'])
+        return item
+
+    # ***看看页卡接口翻页规则全记录***
+    # 1) 如果某一页获取的item数少于21，那么为最后一页
+    # 2） glabalID=617 视频
+    # 3） timestamp第一次为0，后面每次用最后一个item的newstime去更新timestamp留着下次用
+
+    def getNextScrollRequest(self, template, timestamp):
+        return scrapy.Request(
+            url=template % str(timestamp), callback=self.parseScrollPage, meta={'template': template})
+
+    def parseScrollPage(self, response):
+        data = json.loads(response.body)
+        video_list = None
+        if data.get('intro') != '精选':
+            video_list = data.get('list')  # normal formation
+        else:
+            # 精选接口的奇葩设计
+            video_list = []
+            for i in data.get('list', []):
+                for j in i.get('list', []):
+                    video_list.append(j)
+        data = video_list
+        size = len(data)
+        timestamp = 0
+        for video in data:
+            item = self.parseVideoProcess(video)
+            if isinstance(item, dict):
+                timestamp = item.get('newstime')
+                yield item
+            size += 1
+        if timestamp != 0 and timestamp > FILTER_TIME:
+            yield self.getNextScrollRequest(response.meta['template'], timestamp)
